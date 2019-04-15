d760fd21f7ea53fee34fa4985feb8d0f5a1ecbd5
shauryaparashar <shauryaparashar932@gmail.com>
2019-04-16 03:40:29 +0530

    server

---
+++ server1/db.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+var body=13;
+
+var dbconfig = {
+    host: 'localhost',
+    user: 'newuser1',
+    password: 'newpassword',
+    database: 'user'
+};
+
+function todolist(query,cb,params) {
+    var connection = mysql.createConnection(dbconfig);
+    connection.connect();
+    connection.query(query,function(err,data){
+        if(err) throw err;
+        cb(data);
+        console.log(data);
+        connection.end();
+    });
+
+}
+
+module.exports = {
+    todolist
+}
